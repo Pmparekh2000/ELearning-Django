ab92760805a9cd4da4f5fe6dc54e36804a7613a7
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-12-01 14:47:48 +0530

    Added custom ordering field

---
+++ courses/fields.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        # for_fields is the parameter says that for which field is the order required to be calculated
+        self.for_fields = for_fields
+        super(OrderField, self).__init__(*args, **kwargs)
+
+    # The below method is executed before saving the data into the database.
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance, self.attname) is None:
+            # no current value
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    # filter by objects with the same field values
+                    # for the fields in "for_fields"
+                    query = {field: getattr(model_instance, field) for field in self.for_fields}
+                    qs = qs.filter(**query)
+                # get the order of the last item
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instance, self.attrname, value)
+            return value
+        else:
+            return super(OrderField, self).pre_save(model_instance, add)
